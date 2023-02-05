5a15d4e4c9fc5547864bc8eff5ae77b475cf4450
haerin7427 <hae7427@gmail.com>
2023-02-05 18:18:04 +0900

    [prog] 명예의전당 다시풀기

--- Programmers/haerin/LV1/명예의전당.java
+++ Programmers/haerin/LV1/명예의전당.java
@@ -2,7 +2,7 @@
 // title : 명예의전당
 // type : ...
 // time : 10m
-// Created by haerin on 2023-02-02
+// Created by haerin on 2023-02-02, 2023-02-05
 import java.util.*;
 public class 명예의전당 {
     class Solution {
@@ -20,6 +20,24 @@ public class 명예의전당 {
                     Collections.sort(list);
                 }
                 answer[idx++] = list.get(0);
+            }
+            return answer;
+        }
+    }
+    // priorityQueue 사용이 속도가 더 빠름
+    class Solution2 {
+        public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            
+            for(int i=0; i<score.length; i++) {
+                if(pq.size() < k) {
+                    pq.add(score[i]);
+                }else if(pq.peek() < score[i]) {
+                    pq.remove();
+                    pq.add(score[i]);
+                }
+                answer[i] = pq.peek();
             }
             return answer;
         }
