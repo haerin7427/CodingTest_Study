681201423cd0ddb337f798998d74e3602970ad73
haerin7427 <hae7427@gmail.com>
2023-04-25 22:29:01 +0900

    [prog] 동물의아이디와이름

---
+++ Programmers/haerin/SQL/LV1/동물의아이디와이름.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59403
+-- title : 동물의 아이디와 이름
+-- Created by haerin on 2023-04-25
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
