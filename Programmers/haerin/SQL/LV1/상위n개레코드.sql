cd0cffa77329671691cbc0001f456ee1df98559f
haerin7427 <hae7427@gmail.com>
2023-04-26 15:38:48 +0900

    [prog] 상위n개레코드

---
+++ Programmers/haerin/SQL/LV1/상위n개레코드.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- title : 상위 n개 레코드
+-- Created by haerin on 2023-04-26
+SELECT NAME 
+FROM ANIMAL_INS
+ORDER BY DATETIME limit 1;
