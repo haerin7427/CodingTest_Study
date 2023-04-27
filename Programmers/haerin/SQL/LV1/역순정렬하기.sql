a8002dabe2e6c3083f67a9f48d560e0baa0f7daa
haerin7427 <hae7427@gmail.com>
2023-04-27 14:23:16 +0900

    [prog] 역순정렬하기

---
+++ Programmers/haerin/SQL/LV1/역순정렬하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59035
+-- title : 역순 정렬하기
+-- Created by haerin on 2023-04-27
+SELECT NAME, DATETIME 
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
