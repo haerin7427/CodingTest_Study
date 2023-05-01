52ffd3d8f56867062db4ab1ffec494769057afa4
haerin7427 <hae7427@gmail.com>
2023-05-01 12:29:36 +0900

    [prog] 여러기준으로정렬하기기

---
+++ Programmers/haerin/SQL/LV1/여러기준으로정렬하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59404
+-- title : 여러 기준으로 정렬하기
+-- Created by haerin on 2023-05-01
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME ASC, DATETIME DESC;
