53d3f0ede4cc7375ece28bbf8a65027d6489f29a
haerin7427 <hae7427@gmail.com>
2023-05-01 21:57:27 +0900

    [prog] 배달

--- Programmers/haerin/LV2/배달.java
+++ Programmers/haerin/LV2/배달.java
@@ -2,8 +2,8 @@ package Programmers.haerin.LV2;
 // https://school.programmers.co.kr/learn/courses/30/lessons/12978
 // title : 배달
 // type : 플로이드 와샬 알고리즘
-// time : ...
-// Created by haerin on 2023-02-24
+// time : ..., 10m
+// Created by haerin on 2023-02-24, 2023-05-01
 public class 배달 {
     // 정점과 정점 사이의 최소거리 구하기
     class Solution {
@@ -40,6 +40,37 @@ public class 배달 {
             for(int i=1; i<distance[1].length; i++) {
                 if(distance[1][i] <= K)
                     answer += 1;
+            }
+            return answer;
+        }
+    }
+    class Solution2 {
+        public int solution(int N, int[][] road, int K) {
+            int[][] graph = new int[N+1][N+1];
+            for(int y=1; y<=N; y++){
+                for(int x=1; x<=N; x++){
+                    if(x != y) graph[y][x] = 500001;
+                }
+            }
+            for(int[] r : road){
+                graph[r[0]][r[1]] = Math.min(graph[r[0]][r[1]], r[2]);
+                graph[r[1]][r[0]] = Math.min(graph[r[1]][r[0]], r[2]);
+            }
+            for(int k=1; k<=N; k++){
+                for(int y=1; y<=N; y++){
+                    for(int x=1; x<=N; x++){
+                        if(y == k || x == k) continue;
+                        graph[y][x] = Math.min(graph[y][x], graph[y][k] + graph[k][x]);
+    
+                    }
+                }
+            }
+            
+            int answer = 0;
+            for(int i=1; i<=N; i++){
+                if(graph[1][i] <= K){
+                    answer += 1;
+                }
             }
             return answer;
         }
