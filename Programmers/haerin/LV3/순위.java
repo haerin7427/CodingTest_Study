cac5113dc2ae6a8f3d07b62e206c9e1b8562d8ca
haerin7427 <hae7427@gmail.com>
2023-05-05 12:14:15 +0900

    [prog] 순위

--- Programmers/haerin/LV3/순위.java
+++ Programmers/haerin/LV3/순위.java
@@ -2,8 +2,8 @@ package Programmers.haerin.LV3;
 // https://school.programmers.co.kr/learn/courses/30/lessons/49191
 // title : 순위
 // type : 플로이드 와샬
-// time : 18m
-// Created by haerin on 2023-03-31
+// time : 18m, 15m
+// Created by haerin on 2023-03-31, 2023-05-05
 public class 순위 {
     class Solution {
         public int solution(int n, int[][] results) {
@@ -37,6 +37,42 @@ public class 순위 {
                     }
                 }
                 if(cnt == 1) answer += 1;
+            }
+            return answer;
+        }
+    }
+    // 0505 2차시도
+    class Solution2 {
+        public int solution(int n, int[][] results) {
+            int[][] record = new int[n+1][n+1]; // 1 : 승리, -1 : 패배
+            for(int[] r : results){
+                record[r[0]][r[1]] = 1;
+                record[r[1]][r[0]] = -1;
+            }
+            
+            for(int y=1; y<=n; y++){
+                for(int x=1; x<=n; x++){
+                    for(int k=1; k<=n; k++){
+                        if(record[y][k] == 1 && record[k][x] == 1){
+                            record[y][x] = 1;
+                            record[x][y] = -1;
+                        }else if(record[y][k] == -1 && record[k][x] == -1){
+                            record[x][y] = 1;
+                            record[y][x] = -1;
+                        }
+                    }
+                }
+            }
+            int answer = 0;
+            for(int y = 1; y <= n; y++){
+                for(int x=1; x<=n; x++){
+                    if(x == y) continue;
+                    if(record[y][x] == 0){
+                        answer -= 1;
+                        break;
+                    }
+                }
+                answer += 1;
             }
             return answer;
         }
