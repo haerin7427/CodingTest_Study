7f355f4b683253479142942aee99e7b00bf5e892
KimBitnari <nari6726@gmail.com>
2023-05-12 12:19:37 +0900

    [prog] 거스름돈 -> 다시 풀기

--- Programmers/nari/LV3/거스름돈.java
+++ Programmers/nari/LV3/거스름돈.java
@@ -3,7 +3,7 @@
 // DP 대표적인 문제 -> "1,000,000,007로 나눈 나머지"
 
 public class 거스름돈 {
-    class Solution {
+    class Solution1 {
         public int solution(int n, int[] money) {
             long[] dp = new long[n+1];
         
@@ -20,6 +20,26 @@ public class 거스름돈 {
                 }
             }
             
+            return (int)(dp[n] % 1000000007);
+        }
+    }
+
+    class Solution2 {
+        public int solution(int n, int[] money) {
+            // 각 index가 n일 때, 거슬러 줄 방법의 수
+            long[] dp = new long[n+1];
+
+            for(int i=1;i<money.length;i++) {
+                for(int j=0;j<=n;j++) {
+                    // 제일 처음에만 실행되도록
+                    // money[0]을 사용했을 때, 나누어 떨어지는 경우 -> 무조건 1가지수
+                    if(i == 1 && j % money[0] == 0) dp[j] = 1;
+                    
+                    // 해당 index j를 money만큼 뺐을 때, 경우의 수 더하기
+                    if(j >= money[i]) dp[j] += dp[j-money[i]];
+                }
+            }
+
             return (int)(dp[n] % 1000000007);
         }
     }
