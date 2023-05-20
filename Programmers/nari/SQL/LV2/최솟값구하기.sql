156dd7298879dde51e278d5b3cd1b7f7175ea583
KimBitnari <nari6726@gmail.com>
2023-05-20 14:17:52 +0900

    [prog] 최솟값구하기

---
+++ Programmers/nari/SQL/LV2/최솟값구하기.sql
@@ -0,0 +1,10 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59038
+-- title : 최솟값 구하기
+
+SELECT MIN(DATETIME) AS 시간
+FROM ANIMAL_INS;
+
+-- SELECT DATETIME AS 시간
+-- FROM ANIMAL_INS
+-- ORDER BY DATETIME
+-- LIMIT 1;
