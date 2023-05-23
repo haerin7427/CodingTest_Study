762ca98a5f856d2b6457dd955e0887d978f50299
KimBitnari <nari6726@gmail.com>
2023-05-23 10:00:37 +0900

    [leet] DivisorGame

---
+++ Leetcode/nari/Easy/DivisorGame.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/divisor-game/
+// title : 1025. Divisor Game
+// time taken : 3 m 57 s
+
+public class DivisorGame {
+    class Solution {
+        public boolean divisorGame(int n) {
+            return n%2 == 0;
+        }
+    }
+}
