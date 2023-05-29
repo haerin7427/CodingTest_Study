2e589dd0308eb6d2698831efb47866ebb07fd6d3
haerin7427 <hae7427@gmail.com>
2023-05-29 23:42:42 +0900

    [prog] 진료과별총예약횟수출력하기

--- Programmers/haerin/SQL/LV2/진료과별총예약횟수출력하기.sql
+++ Programmers/haerin/SQL/LV2/진료과별총예약횟수출력하기.sql
@@ -1,6 +1,6 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/132202
 -- title : 진료과별 총 예약 횟수 출력하기
--- created by haerin on 2023-0
+-- created by haerin on 2023-05-29
 SELECT MCDP_CD AS 진료과코드, COUNT(*) AS 5월예약건수
 FROM APPOINTMENT
 WHERE APNT_YMD >= '2022-05-01' and APNT_YMD <= '2022-05-31'
